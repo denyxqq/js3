2c0b1376ce462db4b0eb1647691af7c66ca88994
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2024-10-06 20:31:29 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,20 @@
+const email = "visockijdenis@gmail.com";
+console.log(email);
+result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+const userName = "denis";
+const payment = "300грн"
+
+const message = "Дякуєм," + userName + "!" + " До сплати 300 гривень";
+console.log(message)
+let msPayment = confirm(message)
+console.log(msPayment);
+
+const myNm = "My ";
+const myNms = "name ";
+const myIs = "is ";
+const fullName = myNm  + myNms + myIs + "Victor";
+console.log(fullName)
+
